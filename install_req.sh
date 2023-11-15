61481ea74b42e88bf558c70f25315590baad85f4
khashayarghamati <khashayarghamati@gmail.com>
2023-11-15 18:15:22 +0000

    add bash files

--- install_req.sh
+++ install_req.sh
@@ -1,6 +1,6 @@
 unset PYTHONPATH
 python3 -m venv test_venv
 source test_venv/bin/activate
-pip3 install --user --upgrade pip
+pip3 install --upgrade pip
 pip3 install --user -r requirements.txt
 pip3 list
