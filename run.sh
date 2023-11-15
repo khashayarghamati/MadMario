88a7bb9a7c38e41415737467cef8c8a6b601f710
khashayarghamati <khashayarghamati@gmail.com>
2023-11-15 18:48:54 +0000

    add bash files

--- run.sh
+++ run.sh
@@ -23,9 +23,9 @@ echo PBS: PATH = $PBS_O_PATH
 echo ------------------------------------------------------
 
 echo  -n 'Installing requirements'
-CODEPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+#CODEPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 
-pip3 install -r $CODEPATH/requirements.txt
+pip3 install -r "$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"/requirements.txt
 pip3 list
 
 echo ------------------------------------------------------
@@ -34,6 +34,6 @@ eval `/usr/bin/modulecmd tcsh load cuda-10.1`
 
 echo -n 'Running Model'
 #python $PWD/replay.py
-python $CODEPATH/main.py
+python "$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"/main.py
 echo ------------------------------------------------------
 echo Job ends
