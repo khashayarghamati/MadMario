d8071bc0390307c8289fb539693ba830708a698d
khashayarghamati <khashayarghamati@gmail.com>
2023-11-15 18:07:49 +0000

    add bash files

--- install_req.sh
+++ install_req.sh
@@ -1,5 +1,6 @@
 python3 -m venv test_venv
 source test_venv/bin/activate
-pip install --upgrade pip==23.3.1
+pip install --upgrade pip
+pip install --upgrade pip
 pip install -r requirements.txt
 pip list
